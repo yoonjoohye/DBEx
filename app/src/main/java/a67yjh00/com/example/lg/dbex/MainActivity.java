3582634b1494470a43f34689288a0e7d42bc7c9b
yoonjoohye <67yjh00@gmail.com>
2017-06-13 20:33:45 +0900

    #3

--- app/src/main/java/a67yjh00/com/example/lg/dbex/MainActivity.java
+++ app/src/main/java/a67yjh00/com/example/lg/dbex/MainActivity.java
@@ -5,13 +5,36 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editName, editCount, editResultName, editResultCount;
+    Button butInit, butInsert, butSelect;
+    MyDBHelper myHelper;
+    SQLiteDatabase sqldb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editName=(EditText)findViewById(R.id.edit_group_name);
+        editCount=(EditText)findViewById(R.id.edit_group_count);
+        editResultName=(EditText)findViewById(R.id.edit_result_name);
+        editResultCount=(EditText)findViewById(R.id.edit_result_count);
+        butInit=(Button)findViewById(R.id.but_init);
+        butInsert=(Button)findViewById(R.id.but_insert);
+        butSelect=(Button)findViewById(R.id.but_select);
+        //DB 생성
+        myHelper=new MyDBHelper(this);//익명클래스
+        //기존의 테이블이 존재하면 삭제하고 테이블을 새로 생성한다.
+        butInit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqldb=myHelper.getWritableDatabase();
+                myHelper.onUpgrade(sqldb,1,2);
+            }
+        });
     }
     class MyDBHelper extends SQLiteOpenHelper{
         public MyDBHelper(Context context) {
